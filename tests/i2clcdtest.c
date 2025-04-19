52c760fe79635524e1c435ff138cc61ef19cda7e
Mwesigwa-Guma <gumathomas097@gmail.com>
2025-04-19 03:37:17 +0000

    update test file

--- tests/i2clcdtest.c
+++ tests/i2clcdtest.c
@@ -1,16 +1,18 @@
-#include "stdio.h";
+#include "stdio.h"
+#include "sys/ioctl.h"
+#include "err.h"
 
 int main(){
     FILE* fd = fopen("/dev/lcd160200", "w");
     if(fd < 0){
-        printf("could not open device file lcd1602 \n");
+        err(1, "could not open device file lcd1602\n");
         return 0;
     }
 
     fputs("testing driver", fd);
 
     if(ioctl(fd, 0xC0, 0) < 0){
-        preintf("ioctl failure with ");
+        err(1, "ioctl failure with lcd1602");
         fclose(fd);
         return 0;
     }
